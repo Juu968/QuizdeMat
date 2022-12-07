ad24c9a00011a2f240efb295f812a5490d2aae25
Juu968 <110696556+Juu968@users.noreply.github.com>
2022-12-07 17:26:00 -0300

    Add files via upload

--- login.js
+++ login.js
@@ -1,12 +1,12 @@
 //escreva o nome da função. Essa função é chamada quando o botão 'login' é clicado
-function ???() {
+function addUser() {
     //escreva o nome da variável que guardará o nome dos jogadores
-   ??? = document.getElementById("???").value;
-    
+   UserName1 = document.getElementById("player1_name_input").value;
+   UserName2 = document.getElementById("player2_name_input").value;
    //escreva essa variável para que ela seja mostrada no localStorage do navegador
-      localStorage.setItem("player1Name", ???);
-      localStorage.setItem("player2Name", ???);
+      localStorage.setItem("player1Name", UserName1);
+      localStorage.setItem("player2Name", UserName2);
   //escreva o nome da tela que você deseja mostrar quando o botão for clicado
-      window.location = "???";
+      window.location = "tela2.html";
   }
   
